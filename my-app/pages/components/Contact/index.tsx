31f66a9bd939fcf6a1325027e7b92ffcb47edfb5
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2024-04-21 09:22:47 -0300

    update project

--- my-app/pages/components/Contact/index.tsx
+++ my-app/pages/components/Contact/index.tsx
@@ -11,12 +11,25 @@ import ContactMe from "./ContactMe";
 import { motion } from "framer-motion";
 
 export default function Contato() {
-  const [name, setName] = useState("");
-  const [mensagem, setmensagem] = useState("");
-  const [email, setemail] = useState("");
+  const [name, setName] = useState<string>("");
+  const [mensagem, setMensagem] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
 
-  const sendEmail = () => {
-    alert("Ola Mundo");
+  const sendWhatsAppMessage = () => {
+    const mensagemNoWhats = `Nome: ${name}%0AEmail: ${email}%0AMensagem: ${mensagem}`;
+    window.open(
+      `https://wa.me/5571984078620/?text=${mensagemNoWhats}`,
+      "_blank"
+    );
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!name || !email || !mensagem) {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+    sendWhatsAppMessage();
   };
 
   return (
@@ -25,7 +38,7 @@ export default function Contato() {
       whileInView={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
       className="flex flex-col gap-5 p-12"
-      onSubmit={sendEmail}
+      onSubmit={handleSubmit}
     >
       <h1 className="text-white text-4xl font-bold">Contato</h1>
       <p className="text-white">
@@ -38,31 +51,44 @@ export default function Contato() {
       <p className="text-white">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo, tempora.
       </p>
-      <p className="text-white">Nome</p>
+      <label htmlFor="name" className="text-white">
+        Nome
+      </label>
       <input
+        id="name"
         className="text-white bg-neutral-900 rounded-xl outline-none p-4 border-2 border-neutral-700"
         type="text"
         placeholder="Lucas Gabriel"
         value={name}
+        onChange={(e) => setName(e.target.value)}
       />
-      <p className="text-white">E-mail</p>
+      <label htmlFor="email" className="text-white">
+        E-mail
+      </label>
       <input
+        id="email"
         className="text-white bg-neutral-900 rounded-xl outline-none p-4 border-2 border-neutral-700"
         type="text"
         placeholder="lucasgabrielferreirasilva777@gmail.com"
         value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <p className="text-white">Mensagem</p>
+      <label htmlFor="mensagem" className="text-white">
+        Mensagem
+      </label>
       <textarea
+        id="mensagem"
         className="text-white bg-neutral-900 outline-none rounded-xl p-4 border-2 border-neutral-700"
         placeholder="Mensagem"
         value={mensagem}
-        onChange={(e) => sendEmail()}
+        onChange={(e) => setMensagem(e.target.value)}
         name="Mensagem"
         cols={30 as number} // Definindo explicitamente o tipo como number
         rows={10 as number} // Definindo explicitamente o tipo como number
       ></textarea>
-      <button className="text-black bg-white p-2 rounded-xl">Enviar</button>
+      <button type="submit" className="text-black bg-white p-2 rounded-xl">
+        Enviar
+      </button>
     </motion.form>
   );
 }
