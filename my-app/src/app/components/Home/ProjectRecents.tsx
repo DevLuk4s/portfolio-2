635ff2ef86ef8bfe0007c11e5dccb8fbbf074b3c
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2024-04-30 12:53:11 -0300

    update project

--- my-app/src/app/components/Home/ProjectRecents.tsx
+++ my-app/src/app/components/Home/ProjectRecents.tsx
@@ -12,7 +12,9 @@ export default function ProjectRecents() {
   return (
     <div className="flex flex-col gap-10">
       <div className="flex justify-between">
-        <h2 className="text-white text-xl font-semibold">Projetos recentes</h2>
+        <h2 className="text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-semibold">
+          Projetos recentes
+        </h2>
         <Link
           href="components/Project"
           className="group flex justify-between items-center text-white hover:text-violet-500"
@@ -28,14 +30,16 @@ export default function ProjectRecents() {
             className="flex flex-col gap-5 p-5 bg-neutral-900 border-2 border-neutral-700 rounded-xl"
           >
             <Image src={projeto.imagem} alt={projeto.nome} />
-            <h2 className="text-white text-xl font-semibold">{projeto.nome}</h2>
+            <h2 className="text-white sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl font-semibold">
+              {projeto.nome}
+            </h2>
             <p className="text-white">{projeto.descricao}</p>
             <div className="flex justify-between">
               <div className="flex gap-5">
                 {projeto.tecnologias.map((tecnologia, index) => (
                   <Image
                     key={index}
-                    className="w-6"
+                    className="w-5"
                     src={tecnologia}
                     alt={projeto.nome}
                   />
