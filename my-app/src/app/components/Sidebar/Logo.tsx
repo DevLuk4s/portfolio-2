5d12af56c416142f7890b089642b06008b29f76f
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2024-04-30 22:51:56 -0300

    update project

--- my-app/src/app/components/Sidebar/Logo.tsx
+++ my-app/src/app/components/Sidebar/Logo.tsx
@@ -7,7 +7,7 @@ import eu from "@/app/assets/images/slug/eu.jpeg";
 export default function Logo() {
   return (
     <div>
-      <Image className="md:w-28 md:h-28 w-10 h-10 rounded-full" src={eu} alt="eu" />
+      <Image className="md:w-28 md:h-28 w-14 h-14 rounded-full" src={eu} alt="eu" />
       <h2 className="text-white text-xl font-semibold hidden md:block">Lucas Gabriel</h2>
     </div>
   );
