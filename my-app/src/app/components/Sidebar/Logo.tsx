ff25e8aa6e72df6398d5378b69915b5f5532ea6d
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2024-05-06 20:49:06 -0300

    update project

--- my-app/src/app/components/Sidebar/Logo.tsx
+++ my-app/src/app/components/Sidebar/Logo.tsx
@@ -6,7 +6,7 @@ import eu from "@/app/assets/images/slug/eu.jpeg";
 
 export default function Logo() {
   return (
-    <div className="hidden md:block">
+    <div>
       <Image className="w-28 h-28 rounded-full" src={eu} alt="eu" />
       <h2 className="text-white text-xl font-semibold">Lucas Gabriel</h2>
     </div>
--- my-app/src/app/components/Sidebar/page.tsx
+++ my-app/src/app/components/Sidebar/page.tsx
@@ -4,8 +4,16 @@ import MainNavigation from "./MainNavigation";
 import MenuHamburguer from "./MenuHamburguer";
 
 export default function Sidebar() {
+
+  const hamburger = document.querySelector(".hamburger")
+  const SidebarMobile = document.querySelector(".SidebarMobile")
+
+  hamburger?.addEventListener("click", () => {
+    SidebarMobile?.classList.toggle("SidebarMobile")
+  })
+
   return (
-    <aside className="fixed top-0 left-0 overflow-x-hidden z-10 flex flex-col items-center justify-center gap-10 p-12">
+    <aside className="fixed top-0 left-0 overflow-x-hidden z-10 flex flex-col items-center justify-center gap-10 p-12 SidebarMobile">
       <Logo />
       <MainNavigation />
       <MenuHamburguer />
