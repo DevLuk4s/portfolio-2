31732e6cd404963b9ddb9027e2b4770682c75678
Lucas Gabriel <114165245+DevLuk4s@users.noreply.github.com>
2024-05-06 21:43:27 -0300

    update projec

--- my-app/src/app/components/Sidebar/MenuHamburguer.tsx
+++ my-app/src/app/components/Sidebar/MenuHamburguer.tsx
@@ -1,6 +1,6 @@
 export default function MenuHamburguer() {
   return (
-    <div className="fixed top-0 right-0 block md:hidden">
+    <div className="fixed top-0 right-0 block md:hidden p-3">
       <button className="hamburger"></button>
     </div>
   );
